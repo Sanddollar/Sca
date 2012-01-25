6975472ff573b237e7b5aa2fbd8dcec250eac1b5
Christopher Sand <christopher.sand@gmail.com>
2012-01-24 22:30:38 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sca::Application.config.secret_token = '2606576ccb68f71a9551b9729bb013d8c5799236caa6cdd70d72830aae8dad4d25b157d7d81ecf6611196b13a8ab174a183052b827b543d6f814a882c272b313'
